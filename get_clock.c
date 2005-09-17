653cdd1ed457603de14e551adaafc3dbe9f899c3
albert <albert>
2005-09-17 08:21:18 +0000

    please remember endianness

--- get_clock.c
+++ get_clock.c
@@ -6,16 +6,16 @@ gboolean get_clock(GtkWidget* widget,
   unsigned short int buffer[7];
   if (CheckCameraOpen() == FALSE)
     return FALSE;
-  if (ControlMessageRead(0xcc00, (char*)buffer, 14, TIMEOUT) == FALSE) {
+  if (ControlMessageRead(0xcc00, (char*)buffer, sizeof buffer, TIMEOUT) == FALSE) {
     Log("get_clock failed");
     return FALSE;
   }
   printf("Camera time\n");
-  printf("Year: %d\n", buffer[0]);
-  printf("Month: %d\n", buffer[1]);
-  printf("Day: %d\n", buffer[2]);
-  printf("%02d:%02d:%02d\n", buffer[3], buffer[4], buffer[5]);
-  printf("Day of week: %02d\n", buffer[6]);
+  printf("Year: %d\n", le16_to_cpu(buffer[0]));
+  printf("Month: %d\n", le16_to_cpu(buffer[1]));
+  printf("Day: %d\n", le16_to_cpu(buffer[2]));
+  printf("%02d:%02d:%02d\n", le16_to_cpu(buffer[3]), le16_to_cpu(buffer[4]), le16_to_cpu(buffer[5]));
+  printf("Day of week: %02d\n", le16_to_cpu(buffer[6]));
 
   return TRUE;
 }
