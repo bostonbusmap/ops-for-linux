878f505727bbba042fc774b6f4d625e7a29906ad
albert <albert>
2005-09-17 08:23:28 +0000

    need not put that whole string on one line

--- mass_storage.c
+++ mass_storage.c
@@ -8,7 +8,12 @@ gboolean enable_mass_storage(GtkWidget* widget,
   
   if (Monitor("wl 8013dee8 0xa") == TRUE) {
     Log("Sent Mass Storage enable command");
-    MessageBox("Press 'PLAYBACK' on the camcorder to enable Mass Storage mode.\n\nNote: You will need to power-cycle the cam and re-open it to restore\nnormal communications with Ops for linux");
+    MessageBox(
+      "Press 'PLAYBACK' on the camcorder to enable Mass Storage mode.\n"
+      "\n"
+      "Note: You will need to power-cycle the cam and re-open it to restore\n"
+      "normal communications with Ops for linux"
+    );
     return TRUE;
   } else {
     Log("Failed to send Mass Storage Mode enable command");
