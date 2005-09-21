e88927e5fe219f15bacf90c2d5133aebe133f87f
albert <albert>
2005-09-21 03:36:45 +0000

    use current time - adjust computer if you want something weird

--- set_clock.c
+++ set_clock.c
@@ -1,4 +1,7 @@
 #include "ops-linux.h"
+
+#include <time.h>
+
 typedef struct {
   u16 year;
   u16 month;
@@ -12,24 +15,19 @@ gboolean set_clock(GtkWidget* widget,
 		   GdkEvent* event,
 		   gpointer data) {
   set_clock_struct scs;
-  scs.year = cpu_to_le16(2005);
-  scs.month = cpu_to_le16(9);
-  scs.date = cpu_to_le16(20);
-  scs.hour = cpu_to_le16(4);
-  scs.minute = cpu_to_le16(5);
-  scs.second = cpu_to_le16(6);
+  struct tm *t;
+  t = localtime(time(NULL));
+  scs.year = cpu_to_le16(t->tm_year+1900);
+  scs.month = cpu_to_le16(t->tm_mon+1);
+  scs.date = cpu_to_le16(t->tm_mday);
+  scs.hour = cpu_to_le16(t->tm_hour);
+  scs.minute = cpu_to_le16(t->tm_min);
+  scs.second = cpu_to_le16(t->tm_sec);
   if (CheckCameraOpen() == FALSE)
     return FALSE;
   if (ControlMessageWrite(0xcb00, (char*)&scs, sizeof scs, TIMEOUT) == FALSE) {
     Log("set_clock failed");
     return FALSE;
   }
-  /*  printf("Camera time\n");
-  printf("Year: %d\n", le16_to_cpu(buffer[0]));
-  printf("Month: %d\n", le16_to_cpu(buffer[1]));
-  printf("Day: %d\n", le16_to_cpu(buffer[2]));
-  printf("%02d:%02d:%02d\n", le16_to_cpu(buffer[3]), le16_to_cpu(buffer[4]), le16_to_cpu(buffer[5]));
-  printf("Day of week: %02d\n", le16_to_cpu(buffer[6]));*/
-
   return TRUE;
 }
